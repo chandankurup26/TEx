67f81e3937ed1458a60e3323e57755589edc00d2
Chandan Kurup <chandankurup26@gmail.com>
2025-09-07 20:08:14 +0530

    Update text_cleaner.py

--- text_cleaner.py
+++ text_cleaner.py
@@ -7,9 +7,9 @@ txt = extract(news_link)
 
 #text cleaning using gemini_ai
 client = genai.Client()
-
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="Clean the given text and make it into a title and paragraph format: %s" %txt)
+    contents="Clean the given text by removing unnecessary symbols or formatting, then convert it into a properly structured title and paragraph format: %s" %txt)
 
-print(response.text)
+def output():
+    return response.text
