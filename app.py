4a38c0b8ae961fbe65b5c604aafd716e34351645
Chandan Kurup <chandankurup26@gmail.com>
2025-09-23 00:21:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,5 +82,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # ensure tables are created
 
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=True, host="0.0.0.0", port=port)
